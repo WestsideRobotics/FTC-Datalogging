cfad8aaf9167fceb95bd129257ca97ba2c5abff0
WestsideRobotics <westsiderobotics@verizon.net>
2022-02-06 15:17:47 -0800

    update package name to teamcode (for OBJ)

--- Examples/Datalogger.java
+++ Examples/Datalogger.java
@@ -21,7 +21,7 @@ Credit to @Windwoes (https://github.com/Windwoes).
 */
 
 
-package org.firstinspires.ftc.robotcore.external;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerNotifier;
